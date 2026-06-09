e4c7c5bad691c6463081d89fd454f86e7089f443
Muni Prasad Chata <1061689@Kfintech.com>
2026-06-09 15:26:52 +0530

    update

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Welcome to mpctech.in 🚀</h1>
+      <p>Your tools and developer learning platform</p>
+    </div>
+  );
+}
+
+export default App;
