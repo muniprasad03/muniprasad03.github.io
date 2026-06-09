9e418a884fe8bdd33994c946b6201aa26306e289
Muni Prasad Chata <1061689@Kfintech.com>
2026-06-09 16:48:53 +0530

    Added routing and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
+import { Routes, Route, Link } from "react-router-dom";
+
+function Home() {
+  return <h1>Welcome to mpctech.in 🚀</h1>;
+}
+
+function Emi() {
+  return <h1>EMI Calculator</h1>;
+}
+
+function Todo() {
+  return <h1>Todo App</h1>;
+}
+
 function App() {
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Welcome to mpctech.in 🚀</h1>
-      <p>Your tools and developer learning platform</p>
-    </div>
+    <>
+      <nav style={{ padding: "10px", background: "#eee" }}>
+        <Link to="/">Home</Link>{" | "}
+        <Link to="/emi">EMI</Link>{" | "}
+        <Link to="/todo">Todo</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/emi" element={<Emi />} />
+        <Route path="/todo" element={<Todo />} />
+      </Routes>
+    </>
   );
 }
 
